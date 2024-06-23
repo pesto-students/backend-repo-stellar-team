edcd1f6af0698841a182a5e1909c581bfcaf1248
mandar wajage <mandarwajage@gmail.com>
2024-06-23 16:05:04 +0530

    minor update

--- utils/index.js
+++ utils/index.js
@@ -7,8 +7,8 @@ const createJWT = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "none",
+    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    sameSite: "strict", // Prevent CSRF attacks
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
