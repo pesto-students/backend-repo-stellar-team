9234906ffb5391e3ff98e9b65781dcbb3d03b792
mandar wajage <mandarwajage@gmail.com>
2024-06-23 13:20:27 +0530

    change in createJWT function

--- utils/index.js
+++ utils/index.js
@@ -7,8 +7,8 @@ const createJWT = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
