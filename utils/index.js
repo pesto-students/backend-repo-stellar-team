86909c55b4749e7f134b15e03ac70f73209faada
mandar wajage <mandarwajage@gmail.com>
2024-06-26 00:57:04 +0530

    monir updates

--- utils/index.js
+++ utils/index.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
 
 const createJWT = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1d' });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'none',
     domain: 'stellar-task-manager.netlify.app', // Use your domain here
-    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
+    maxAge: 3600000, // 1 h
   });
 };
 
