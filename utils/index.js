19d761fdaf429ac840be5de01def2c4aba943ff5
mandar wajage <mandarwajage@gmail.com>
2024-06-26 00:11:11 +0530

    updated createJWT function

--- utils/index.js
+++ utils/index.js
@@ -1,16 +1,15 @@
-import jwt from "jsonwebtoken";
+const jwt = require('jsonwebtoken');
 
 const createJWT = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
-  res.cookie("token", token, {
+  res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'none',
+    domain: 'stellar-task-manager.netlify.app', // Use your domain here
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
 
-export default createJWT;
+module.exports = createJWT;
