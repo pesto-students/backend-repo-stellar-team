e1ba0b12ae553b162b7060003ca17dfb7a36e1c2
mandar wajage <mandarwajage@gmail.com>
2024-06-26 21:10:15 +0530

    change sorder of functions in middleware

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ dbConnection();
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cookieParser());
 
 app.use(
   cors({
@@ -27,7 +28,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser());
 
 app.use(morgan("dev"));
 app.use("/api", routes);
